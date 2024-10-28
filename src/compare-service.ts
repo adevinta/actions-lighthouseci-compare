c6eda49277827ef6f1ce8bfe13c583e179dac93b
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2024-10-28 16:14:55 +0100

    format code

--- src/compare-service.ts
+++ src/compare-service.ts
@@ -63,15 +63,17 @@ export const compareLHRs = ({
       const performance: Result.Category = runLHR.categories.performance
       const ancestorPerformance: Result.Category =
         ancestorRunLHR.categories.performance
-      const currentPerformance = (
-        (performance.score ? performance.score : 0) * 100
-      ).toFixed(0)
-      const previousPerformance = (
-        (ancestorPerformance.score ? ancestorPerformance.score : 0) * 100
-      ).toFixed(0)
-      const diffPerformance = (
-        currentPerformance - previousPerformance
-      ).toFixed(0)
+      const currentPerformance = parseFloat(
+        ((performance.score ? performance.score : 0) * 100).toFixed(0)
+      )
+      const previousPerformance = parseFloat(
+        (
+          (ancestorPerformance.score ? ancestorPerformance.score : 0) * 100
+        ).toFixed(0)
+      )
+      const diffPerformance = parseFloat(
+        (currentPerformance - previousPerformance).toFixed(0)
+      )
       const isPerformanceRegression = diffPerformance < 0
       const lcp: AuditResult = runLHR.audits['largest-contentful-paint']
       const ancestorLCP: AuditResult =
