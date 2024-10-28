3db088dcdf5440619912fd1c519a226412d6c8ce
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2024-10-28 16:11:54 +0100

    refactor to adjust to code style

--- src/compare-service.ts
+++ src/compare-service.ts
@@ -63,11 +63,15 @@ export const compareLHRs = ({
       const performance: Result.Category = runLHR.categories.performance
       const ancestorPerformance: Result.Category =
         ancestorRunLHR.categories.performance
-      const currentPerformance =
-        ((performance.score ? performance.score : 0) * 100).toFixed(0)
-      const previousPerformance =
-        ((ancestorPerformance.score ? ancestorPerformance.score : 0) * 100).toFixed(0)
-      const diffPerformance = (currentPerformance - previousPerformance).toFixed(0)
+      const currentPerformance = (
+        (performance.score ? performance.score : 0) * 100
+      ).toFixed(0)
+      const previousPerformance = (
+        (ancestorPerformance.score ? ancestorPerformance.score : 0) * 100
+      ).toFixed(0)
+      const diffPerformance = (
+        currentPerformance - previousPerformance
+      ).toFixed(0)
       const isPerformanceRegression = diffPerformance < 0
       const lcp: AuditResult = runLHR.audits['largest-contentful-paint']
       const ancestorLCP: AuditResult =
